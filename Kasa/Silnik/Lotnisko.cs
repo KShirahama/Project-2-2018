5de86dd8aa6ec9d136b9dce265832f7046c89d55
Kenichi Shirahama <keniichii@gmail.com>
2018-05-17 12:54:29 +0200

    Init

---
+++ Kasa/Silnik/Lotnisko.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silnik
+{
+    public class Lotnisko
+    {
+        private String nazwa;
+        private List<Samolot> samoloty;
+
+        public Lotnisko(String nazwa)
+        {
+            this.nazwa = nazwa;
+            samoloty = new List<Samolot>();
+        }
+    }
+}
---
+++ Kasa/Silnik/Samolot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silnik
+{
+    public class Samolot
+    {
+        private String nazwa;
+        private int ID, zasieg, liczbaMiejsc;
+
+        public Samolot(String nazwa, int ID, int zasieg, int liczbaMiejsc)
+        {
+            this.nazwa = nazwa;
+            this.ID = ID;
+            this.zasieg = zasieg;
+            this.liczbaMiejsc = liczbaMiejsc;
+        }
+    }
+}
